0683df3dcdff2876782f92f2a876716bdc4f145f
Haroun Ach <harounacho21@gmail.com>
2022-03-12 13:16:27 +0100

    Update dashboard page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import classNames from "classnames";
 import styles from "./dashboard.module.scss";
 
 import Appbar from "../components/Appbar/Appbar";
+import Button from "../components/Button/Button";
 
 const Dashboard: NextPage = () => {
   const logoStyles = {
@@ -11,7 +13,7 @@ const Dashboard: NextPage = () => {
   };
 
   return (
-    <div className={styles.container}>
+    <div className={classNames(styles.page)}>
       <Head>
         <title>NoteShar Dashboard</title>
         <meta name="description" content="NoteShar Dashboard" />
@@ -19,107 +21,165 @@ const Dashboard: NextPage = () => {
       </Head>
 
       {/* Appbar */}
-      <Appbar />
+      <Appbar customClasses={classNames(styles.page__header, "container")} />
 
-      <main className="main">
-        <h1 className="title">Username Dashboard</h1>
-        <p>Create and share notes about your life</p>
-        <div className="note-dialog">
-          <form className="note-form">
-            <div className="note-form__title-wrapper">
-              <h2 className="note-form__title">Create Note</h2>
-            </div>
-            <div className="note-form__control">
-              <label htmlFor="note-title" className="note-form__label">
-                Title
-              </label>
-              <input
-                className="note-form__input"
-                type="text"
-                id="note-title"
-                name="note_title"
-                placeholder="Title"
-              />
-              <p className="note-form__error">Note Title Error</p>
-            </div>
-            <div className="note-form__control">
-              <label htmlFor="note-description" className="note-form__label">
-                Note Description
-              </label>
-              <textarea
-                className="note-form__area"
-                name="note_description"
-                id="note-description"
-                placeholder="Description"
-                cols={30}
-                rows={10}
-              ></textarea>
-              <p className="note-form__error">Note Description Error</p>
-            </div>
-            <div className="note-form__submit-wrapper">
-              <button className="note-form__submit">Save</button>
-            </div>
-            <div className="note-form__submit-wrapper">
-              <button className="note-form__submit">Cancel</button>
-            </div>
-          </form>
-        </div>
-        <div className="notes">
-          {/* Note 1 */}
-          <div className="note">
-            <div className="note__row">
-              <span className="note__title">Title</span>
-              <span className="note__date">06/03/2022</span>
-            </div>
+      <main className={classNames("main", styles.page__main)}>
+        <div className="container">
+          <h1 className="title">Username Dashboard</h1>
+          <p>Create and share notes about your life</p>
+          <div className={classNames(styles.page__center)}>
+            <form className="form">
+              <div className="form__section">
+                <h2 className="form__title">Create Note</h2>
+              </div>
+              <div className="form__section input-control">
+                <label htmlFor="note-title" className="input-control__label">
+                  Title
+                </label>
+                <input
+                  className="input-control__input"
+                  type="text"
+                  id="note-title"
+                  name="note_title"
+                  placeholder="Title"
+                />
+                <p className="input-control__error">Note Title Error</p>
+              </div>
+              <div className="form__section input-control">
+                <label
+                  htmlFor="note-description"
+                  className="input-control__label"
+                >
+                  Note Description
+                </label>
+                <textarea
+                  className="input-control__area"
+                  name="note_description"
+                  id="note-description"
+                  placeholder="Description"
+                  cols={30}
+                  rows={10}
+                ></textarea>
+                <p className="input-control__error">Note Description Error</p>
+              </div>
+              <div className="form__section">
+                <div className="form__btn-group">
+                  <Button customClasses={classNames("note-form__submit")}>
+                    Save
+                  </Button>
 
-            <div className="note__row">
-              <p className="note__description">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Reiciendis nulla natus molestias.
-              </p>
-            </div>
-            <div className="note__row">
-              <span className="note__createdby">
-                Created By <span className="note__username">UserName</span>
-              </span>
-              <div className="note__actions">
-                <i className="note__btn fa-solid fa-thumbs-down"></i>
-                <i className="note__btn fa-solid fa-thumbs-up"></i>
-                <i className="note__btn fa-solid fa-share-nodes"></i>
-                <i className="note__btn fa-solid fa-trash"></i>
-                <i className="note__btn fa-solid fa-pen"></i>
+                  <Button
+                    variant="cancel"
+                    customClasses={classNames("note-form__submit")}
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="notes">
+            {/* Note 1 */}
+            <div className="note">
+              <div className="note__row">
+                <span className="note__title">Note Title by Username</span>
+                <span className="note__date">06/03/2022</span>
+              </div>
+
+              <div className="note__row">
+                <p className="note__description">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Voluptatum natus quis aliquid blanditiis neque? Tenetur dolor
+                  et architecto odio debitis.
+                </p>
+              </div>
+              <div className="note__row">
+                <span className="note__createdby">
+                  Created By <span className="note__username">UserName</span>
+                </span>
+                <div className="note__actions">
+                  <button className="note__btn btn btn--icon-btn">
+                    <i className="btn__icon fa-solid fa-thumbs-down"></i>
+                  </button>
+                  <button className="note__btn btn btn--icon-btn">
+                    <i className="btn__icon fa-solid fa-thumbs-up"></i>
+                  </button>
+                  <button className="note__btn btn btn--icon-btn">
+                    <i className="btn__icon fa-solid fa-share-nodes"></i>
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* Note 2 */}
-          <div className="note">
-            <div className="note__row">
-              <span className="note__title">Title</span>
-              <span className="note__date">06/03/2022</span>
+            {/* Note 2 */}
+            <div className="note">
+              <div className="note__row">
+                <span className="note__title">Note Title by Username</span>
+                <span className="note__date">06/03/2022</span>
+              </div>
+
+              <div className="note__row">
+                <p className="note__description">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Voluptatum natus quis aliquid blanditiis neque? Tenetur dolor
+                  et architecto odio debitis.
+                </p>
+              </div>
+              <div className="note__row">
+                <span className="note__createdby">
+                  Created By <span className="note__username">UserName</span>
+                </span>
+                <div className="note__actions">
+                  <button className="note__btn btn btn--icon-btn">
+                    <i className="btn__icon fa-solid fa-thumbs-down"></i>
+                  </button>
+                  <button className="note__btn btn btn--icon-btn">
+                    <i className="btn__icon fa-solid fa-thumbs-up"></i>
+                  </button>
+                  <button className="note__btn btn btn--icon-btn">
+                    <i className="btn__icon fa-solid fa-share-nodes"></i>
+                  </button>
+                </div>
+              </div>
             </div>
 
-            <div className="note__row">
-              <p className="note__description">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Reiciendis nulla natus molestias.
-              </p>
-            </div>
-            <div className="note__row">
-              <span className="note__createdby">
-                Created By <span className="note__username">UserName</span>
-              </span>
-              <div className="note__actions">
-                <i className="note__btn fa-solid fa-thumbs-down"></i>
-                <i className="note__btn fa-solid fa-thumbs-up"></i>
-                <i className="note__btn fa-solid fa-share-nodes"></i>
-                <i className="note__btn fa-solid fa-trash"></i>
-                <i className="note__btn fa-solid fa-pen"></i>
+            {/* Note 3 */}
+            <div className="note">
+              <div className="note__row">
+                <span className="note__title">Note Title by Username</span>
+                <span className="note__date">06/03/2022</span>
+              </div>
+
+              <div className="note__row">
+                <p className="note__description">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Voluptatum natus quis aliquid blanditiis neque? Tenetur dolor
+                  et architecto odio debitis.
+                </p>
+              </div>
+              <div className="note__row">
+                <span className="note__createdby">
+                  Created By <span className="note__username">UserName</span>
+                </span>
+                <div className="note__actions">
+                  <button className="note__btn btn btn--icon-btn">
+                    <i className="btn__icon fa-solid fa-thumbs-down"></i>
+                  </button>
+                  <button className="note__btn btn btn--icon-btn">
+                    <i className="btn__icon fa-solid fa-thumbs-up"></i>
+                  </button>
+                  <button className="note__btn btn btn--icon-btn">
+                    <i className="btn__icon fa-solid fa-share-nodes"></i>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </main>
+      <footer className={classNames("footer", styles.page__footer)}>
+        <p className="footer__notice">Copyright Haroun Ach 2022</p>
+      </footer>
     </div>
   );
 };
